ec79fd9836abe34b458441864495391c8ca81982
jcruiz22 <jcruizm22@gmail.com>
2024-11-28 22:43:54 +0100

    Implement user management API with MongoDB integration

---
+++ app/api/(auth)/users/route.ts
@@ -0,0 +1,85 @@
+import connect from "@/lib/db";
+import User from "@/lib/modals/user";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+
+const ObjectId = require("mongoose").Types.ObjectId;
+
+export const GET = async () => {
+  try {
+    await connect();
+    const users = await User.find({});
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error) {
+    return new NextResponse("Error fetching users", { status: 500 });
+  }
+};
+
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    await connect();
+    const user = new User(body);
+    await user.save();
+    return new NextResponse(JSON.stringify(user), { status: 201 });
+  } catch (error) {
+    return new NextResponse("Error creating user", { status: 500 });
+  }
+}
+export const PATCH = async (request: Request) => {
+  try {
+    const body = await request.json();
+    const { userId, newUserName } = body;
+    await connect();
+ 
+    if (!userId || !newUserName) {
+      return new NextResponse("User not found", { status: 404 });
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse("Invalid user id", { status: 400 });
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: new ObjectId(userId) },
+      { username: newUserName },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return new NextResponse("User not found", { status: 404 });
+    }
+
+    return new NextResponse(JSON.stringify(updatedUser), { status: 200 });
+
+  } catch (error) {
+    return new NextResponse("Error updating user", { status: 500 });
+  }
+};
+
+export const DELETE = async (request: Request) => {
+  try {
+    const body = await request.json();
+    const { userId } = body;
+    await connect();
+
+    if (!userId) {
+      return new NextResponse("User not found", { status: 404 });
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse("Invalid user id", { status: 400 });
+    }
+
+    const deletedUser = await User.findOneAndDelete({ _id: new ObjectId(userId) });
+
+    if (!deletedUser) {
+      return new NextResponse("User not found", { status: 404 });
+    }
+
+    return new NextResponse(JSON.stringify(deletedUser), { status: 200 });
+
+  } catch (error) {
+    return new NextResponse("Error deleting user", { status: 500 });
+  }
+};
